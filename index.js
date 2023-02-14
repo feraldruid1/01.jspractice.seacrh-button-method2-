3204e2eb4d773c05c4b1d5a0b7f17f72eb4dfbc0
nikoloz kakabadze <nk360dk@gmail.com>
2023-02-14 11:22:33 -0800

    practice

---
+++ index.js
@@ -0,0 +1,9 @@
+let elem=document.querySelector("input");
+function button(){
+    elem.style.width=700+"px";
+    elem.style.height=150+"px";
+}
+function buttonclick(){
+    elem.style.width=0+"px"
+    elem.style.height=0+"px";
+}
