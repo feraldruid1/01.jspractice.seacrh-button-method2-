f11ea10d566a763678d5755d05ead1a8e2ca2f84
nikoloz kakabadze <nk360dk@gmail.com>
2023-02-15 04:42:28 -0800

    practice

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let elem=document.querySelector("input");
-function onclick(){
+function onick(){
 if(elem.width===0){
     elem.style.width=700+"px";
     elem.style.height=150+"px";
-}
-if(elem.width===700){
-    elem.style.width=0+"px";
-    elem.style.height=0+"px";
+    console.log("asdf")
+}else{  
+    elem.style.width=700-"px";
+    elem.style.height=150-"px";
 }
 }
