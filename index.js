0acd55febc53683ff308e19abc0ba7e4e602f830
nikoloz kakabadze <nk360dk@gmail.com>
2023-02-16 01:01:25 -0800

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let elem=document.querySelector("input");
+let elem2=document.querySelector("div")
 console.log(elem.width)
+maindiv.addEventListener("click",onick)
 function onick(){
     elem.classList.toggle("active")
+    elem2.classList.toggle("divborder")
 }
